872b843a9d7139f4a706711776e6f1fa3e74bca3
Adrian Ghiuta <ady.ghiuta@gmail.com>
2017-07-05 02:21:31 +0300

    attempt to get Heroku to flush cache

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="plueprint",
     description="API Blueprint (https://apiblueprint.org/) parser in pure "
                 "Python",
-    version="0.4.2",
+    version="0.4.3",
     license="New BSD",
     author="Vadim Markovtsev",
     author_email="gmarkhor@gmail.com",
